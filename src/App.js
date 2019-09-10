e3850e8ae29af32e72f875f18680f4f08af347b5
Edson <edsonlessajunior@gmail.com>
2019-09-10 14:00:04 -0300

    segundo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MenuSuperior from "./components/MenuSuperior/MenuSuperior";
+
+function App() {
+  return (
+    <div>
+        <MenuSuperior />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+import './Navbar.css';
+
+
+class Navbar extends Component{
+    render() {
+        return(
+            <nav>
+                <ol>
+                    <li>
+
+                    </li>
+                </ol>
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
